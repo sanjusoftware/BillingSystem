e55751bc9f937ea9c255393d3af3ffd71e8f9c27
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2012-04-25 21:28:33 +0530

    added user tests as well now!!

--- tests/com/mckinsey/billingsystem/user/UserTest.java
+++ tests/com/mckinsey/billingsystem/user/UserTest.java
@@ -1,5 +1,7 @@
 package com.mckinsey.billingsystem.user;
 
+import junit.framework.Assert;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 /**
@@ -12,6 +14,9 @@ import org.junit.Test;
 public class UserTest {
     @Test
     public void testGetDiscountPercentage() throws Exception {
-
+        Assert.assertEquals(30.0, new Employee(new DateTime()).getDiscountPercentage());
+        Assert.assertEquals(10.0, new Affiliate(new DateTime()).getDiscountPercentage());
+        Assert.assertEquals(0.0, new Customer(new DateTime()).getDiscountPercentage());
+        Assert.assertEquals(5.0, new Customer(new DateTime(2009, 1, 1, 0, 0, 0, 0)).getDiscountPercentage());
     }
 }
